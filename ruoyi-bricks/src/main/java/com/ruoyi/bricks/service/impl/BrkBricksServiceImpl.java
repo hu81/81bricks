5a6e2a8275bcf429d6f828f818b0503e64538ff8
Jackie.Hu <Jackie.Hu@wh.augmentum.com>
2026-03-19 17:54:20 +0800

    fix: removeAbnormalParts增加qz开头的零件过滤

--- ruoyi-bricks/src/main/java/com/ruoyi/bricks/service/impl/BrkBricksServiceImpl.java
+++ ruoyi-bricks/src/main/java/com/ruoyi/bricks/service/impl/BrkBricksServiceImpl.java
@@ -283,7 +283,7 @@ public class BrkBricksServiceImpl implements IBrkBricksService
             if (removeAbnormalParts && processedBrick.getPartNumber() != null)
             {
                 String partNumber = processedBrick.getPartNumber();
-                if (partNumber.contains("_") || partNumber.startsWith("qm"))
+                if (partNumber.contains("_") || partNumber.startsWith("qm") || partNumber.startsWith("qz"))
                 {
                     continue;
                 }
